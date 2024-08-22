07efc5229dc85262a3a0b4a249e8941b563f18d2
lvlh2 <lvlh2@outlook.com>
2024-08-22 20:39:53 +0800

    Update fetch_bilibili_comments.py

--- fetch_bilibili_comments.py
+++ fetch_bilibili_comments.py
@@ -19,7 +19,7 @@ from lxml import etree
 
 # NOTE: Please configure your Cookie here.
 HEADERS = {
-    # 'cookie': 'Your Cookie',
+    'cookie': 'Your Cookie',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0',
 }
 
